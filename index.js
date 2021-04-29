2f8a2bb0b89c023d2de3d46386cbdddfdd9a2663
ivetvrobel <iveta.vrobelova@gmail.com>
2021-04-29 20:04:24 +0200

    Hotový javascript

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ console.log('funguju');
   const tshirtElm = document.querySelector('#product-image');
 
   tshirtElm.style = `fill: ${}`
-
 }
 
 document.querySelectorAll('button').addEventListener('click', changeColor) */
@@ -16,9 +15,31 @@ document.querySelectorAll('button').addEventListener('click', changeColor) */
 document.querySelector('.white').addEventListener('click', () => {
     document.querySelector('#product-image').style = 'fill: white';
     document.querySelector('.white').disabled = true;
+    document.querySelector('.black').disabled = false;
+    document.querySelector('.red').disabled = false;
+    document.querySelector('.skyBlue').disabled = false;
 });
 
 document.querySelector('.black').addEventListener('click', () => {
     document.querySelector('#product-image').style = 'fill: black';
     document.querySelector('.black').disabled = true;
+    document.querySelector('.white').disabled = false;
+    document.querySelector('.red').disabled = false;
+    document.querySelector('.skyBlue').disabled = false;
+});
+
+document.querySelector('.red').addEventListener('click', () => {
+    document.querySelector('#product-image').style = 'fill: red';
+    document.querySelector('.red').disabled = true;
+    document.querySelector('.black').disabled = false;
+    document.querySelector('.white').disabled = false;
+    document.querySelector('.skyBlue').disabled = false;
+});
+
+document.querySelector('.skyBlue').addEventListener('click', () => {
+    document.querySelector('#product-image').style = 'fill: skyBlue';
+    document.querySelector('.skyBlue').disabled = true;
+    document.querySelector('.red').disabled = false;
+    document.querySelector('.black').disabled = false;
+    document.querySelector('.white').disabled = false;
 });
