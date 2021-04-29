55df17b3dc1ac8c18a58e2e0991bac4697b2108a
ivetvrobel <iveta.vrobelova@gmail.com>
2021-04-29 19:36:19 +0200

    Úprava javascriptu

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
 'use strict';
 
 console.log('funguju');
+
+// style="fill: #a6a6a6"
+/* const changeColor = (event) => {
+
+  const tshirtElm = document.querySelector('#product-image');
+
+  tshirtElm.style = `fill: ${}`
+
+}
+
+document.querySelectorAll('button').addEventListener('click', changeColor) */
+
+document.querySelector('.white').addEventListener('click', () => {
+    document.querySelector('#product-image').style = 'fill: white';
+    document.querySelector('.white').disabled = true;
+});
+
+document.querySelector('.black').addEventListener('click', () => {
+    document.querySelector('#product-image').style = 'fill: black';
+    document.querySelector('.black').disabled = true;
+});
